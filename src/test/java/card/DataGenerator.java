d858e61242582737ed97d276b1de6774bb022bcf
Kirillova Olesya <olesyak-85@mail.ru>
2021-11-22 20:47:29 +0300

    added data-class and tests

---
+++ src/test/java/card/DataGenerator.java
@@ -0,0 +1,43 @@
+package card;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+
+    DataGenerator() {
+    }
+
+    private Faker faker = new Faker(new Locale("ru"));
+
+    public String getDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public String getCity() {
+        String[] city = {
+                "Саратов","Кемерово", "Майкоп", "Москва",
+                "Симферополь", "Смоленск", "Тамбов", "Санкт-Петербург",
+                "Калининград", "Новокузнецк", "Ростов-на-Дону",
+                "Анапа", "Волгоград", "Владивосток", "Мурманск"};
+        int rnd = new Random().nextInt(city.length);
+        return city[rnd];
+    }
+
+    public String getName() {
+
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public String getPhone() {
+
+        return faker.numerify("+79#########");
+    }
+
+}
+
+
---
+++ src/test/java/card/TestCardDelivery.java
@@ -0,0 +1,115 @@
+package card;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestCardDelivery {
+
+    private DataGenerator dataGenerator = new DataGenerator();
+    String city = dataGenerator.getCity();
+    String name = dataGenerator.getName();
+    String phone = dataGenerator.getPhone();
+
+    @BeforeEach
+    void setupClass() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldSendValidValueAndChangeDate() {
+        $("[data-test-id=city] .input__control").sendKeys(city);
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(dataGenerator.getDate(5));
+        $("[data-test-id=name] .input__control").sendKeys(name);
+        $("[data-test-id=phone] .input__control").sendKeys(phone);
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[class=button__text]").click();
+        $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=success-notification]  .notification__title")
+                .shouldHave(exactText("Успешно!"));
+        $("[data-test-id=success-notification]  .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + dataGenerator.getDate(5)));
+
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(dataGenerator.getDate(7));
+        $("[class=button__text]").click();
+        $("[data-test-id=replan-notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=replan-notification] .notification__title")
+                .shouldHave(exactText("Необходимо подтверждение"));
+        $(byText("Перепланировать")).click();
+        $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=success-notification]  .notification__title")
+                .shouldHave(exactText("Успешно!"));
+        $("[data-test-id=success-notification]  .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + dataGenerator.getDate(7)));
+    }
+
+    @Test
+    void shouldSendEmptyForm() {
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[class=button__text]").click();
+        $(withText("Поле обязательно для заполнения"))
+                .shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSendInvalidCity() {
+        $("[data-test-id=city] .input__control").sendKeys("Petersburg");
+        $("[class=button__text]").click();
+        $("[data-test-id=city].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldSendInvalidName() {
+
+        $("[data-test-id=city] .input__control").sendKeys(city);
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(dataGenerator.getDate(5));
+        $("[data-test-id=name] .input__control").sendKeys("Николай Илья Харитон Ульяна Яков");
+        $("[data-test-id=phone] .input__control").sendKeys(phone);
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[class=button__text]").click();
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSendValidNameWithЁ() {
+        $("[data-test-id=city] .input__control").sendKeys(city);
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(dataGenerator.getDate(5));
+        $("[data-test-id=name] .input__control").sendKeys("Лёвин Андрей");
+        $("[data-test-id=phone] .input__control").sendKeys(phone);
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[class=button__text]").click();
+        $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=success-notification]  .notification__title")
+                .shouldHave(exactText("Успешно!"));
+        $("[data-test-id=success-notification]  .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + dataGenerator.getDate(5)));
+    }
+
+    @Test
+    void shouldSendInvalidPhoneNumber(){
+        $("[data-test-id=city] .input__control").sendKeys(city);
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(dataGenerator.getDate(5));
+        $("[data-test-id=name] .input__control").sendKeys(name);
+        $("[data-test-id=phone] .input__control").sendKeys("+79321");
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[class=button__text]").click();
+        $("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+}
